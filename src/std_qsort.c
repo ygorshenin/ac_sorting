dd5de3491cbe1dbf8e14c4dc1b93c6e185f1f48b
ygorshenin <ygorshenin@google.com>
2011-10-11 17:06:27 +0400

    Simplified comparator

--- src/std_qsort.c
+++ src/std_qsort.c
@@ -7,12 +7,7 @@ static int cmp(const void *_lhs, const void *_rhs) {
   int lhs = * (const int *) _lhs;
   int rhs = * (const int *) _rhs;
 
-  if (lhs < rhs)
-    return -1;
-  else if (lhs == rhs)
-    return 0;
-  else
-    return +1;
+  return (lhs > rhs) - (lhs < rhs);
 }
 
 void std_qsort(size_t n, int *array) {
